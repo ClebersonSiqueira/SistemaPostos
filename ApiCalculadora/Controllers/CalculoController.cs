a839dc6509a6e3b8edbbc3901b7e88e83ccc9f43
clebersonsiqueira <cleberson.siqueira17@gmail.com>
2021-01-27 22:35:29 -0300

    first commit

---
+++ ApiCalculadora/Controllers/CalculoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiCalculadora.Services;
+
+namespace ApiCalculadora.Controllers
+{
+    public class CalculoController
+    {
+        private readonly CalculoService _CalculoService;
+        public void RecebeValores (double KmPorGas, double KmPorEtanol, double valorGas, double valorEtanol)
+        {
+            double resultadoDouble = _CalculoService.KmLitro(KmPorGas, KmPorEtanol, valorGas);
+            string resultadoString = _CalculoService.retornoFrase(resultadoDouble, valorEtanol);
+        }
+    }
+}
---
+++ ApiCalculadora/Services/CalculoService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCalculadora.Services
+{
+    public class CalculoService
+    {
+
+        public double KmLitro(double KmPorGas, double KmPorEtanol, double valorGas)
+        {
+            double divKms =  KmPorEtanol / KmPorGas;
+            double valorDoCalculo = divKms * valorGas;
+            return valorDoCalculo;
+        }
+
+        public string retornoFrase(double valorDoCalculo, double valorEtanol)
+        {
+            if (valorEtanol <= valorDoCalculo)
+                return "Compensa abastecer com o Etanol";
+            else
+                return "Compensa abastecer com a Gasolina";
+        }
+
+
+    }
+}
